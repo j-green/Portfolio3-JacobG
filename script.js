6935a1e085051667c7e4df0cf304c24a65b76bb1
Jacob Green <jacobcgreen@verizon.net>
2015-01-15 14:03:39 -0800

    Made superheros

--- script.js
+++ script.js
@@ -7,8 +7,53 @@
 
 $("document").ready(function(){
     $(".practice").css("background-color", "yellow");
-    $('p:first').css({"background-color": "yellow", "color": "white" });
+    
+    $('#superhumans').accordion({header:"h3"});
+    $('superhumans').css('width', '400px');
+    
+    $('h3').css('background', 'cyan')
+    
+    
+    
+    $('p:first').css({"background-color": "yellow", "color": "black" });
+    
      $('p:last').css({"background-color": "purple", "color": "white" });
+     
+     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+     
+     $('div').bind('mouseover',  mouseOverMe());
+   
+     $('h1').bind('click', mouseClick);
+     
+     $('#replaceWText').bind('click', replaceWText);
+     
+     $('#randPara').bind('click', addAPara);
+     
+     $('#removePara').bind('click', removeAPara);
 });
 
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+
+
+function addAPara(){
+    $('#randPara').append('<p>ADDED</p>');
+}
+
+function replaceWText(){
+    $('#replaceWText').text('Replaced!');
+}
+
+function mouseOverMe(){
+    $("h1").html("Joshua is lame");
+    }
+    
+function mouseOutMe(){
+        $('h1').html('Nick is cool');
+    }
+    
+function mouseClick(){
+        $('p').html('Clicked!!!!');
+    }
 
